b7f4f67859e725fdb59a2f80e0a9f9c77283f1a6
Aleksandr Zurnachyan <aleksandrzurnachyan@MacBook-Air-Aleksandr.local>
2025-12-04 16:04:54 +0400

    added confirmation page refactor

--- erp_hdm_armenia_pos/static/src/screens/confrimation_page.js
+++ erp_hdm_armenia_pos/static/src/screens/confrimation_page.js
@@ -5,22 +5,32 @@ import { useService } from "@web/core/utils/hooks";
 import { patch } from "@web/core/utils/patch";
 import { ConfirmationPage } from "@pos_self_order/app/pages/confirmation_page/confirmation_page";
 import { OrderReceipt } from "@point_of_sale/app/screens/receipt_screen/receipt/order_receipt";
+import { rpc } from "@web/core/network/rpc";
 
 patch(ConfirmationPage.prototype, {
-
+     get printOptions() {
+        const result = super.printOptions;
+        result.webPrintFallback = true;
+        return result;
+     },
      async printOrder() {
         if (this.selfOrder.config.self_ordering_mode === "kiosk" && this.canPrintReceipt()) {
             try {
                 this.isPrinting = true;
                 const order = this.confirmedOrder;
-                console.log(order);
-                const result = await this.printer.print(
-                    OrderReceipt,
-                    {
-                        order: order,
-                    },
-                    this.printOptions
-                );
+                var result = false
+                if (this.printOptions?.webPrintFallback){
+                    window.print()
+                    result = true
+                } else {
+                    result = await this.printer.print(
+                        OrderReceipt,
+                        {
+                            order: order,
+                        },
+                        this.printOptions
+                    );
+                }
                 if (!this.selfOrder.has_paper) {
                     this.updateHasPaper(true);
                 }
@@ -46,7 +56,6 @@ patch(ConfirmationPage.prototype, {
                     console.error(e);
                 }
             } finally {
-                window.print();
                 this.isPrinting = false;
             }
         }
